8759f11ecc70b890f811b47058ae6f133c05f163
Alan McCosh <amccosh@getwellnetwork.com>
2018-01-26 00:32:47 -0800

    basic input routing and response in place

--- Assets/scripts/DemoCharacter.cs
+++ Assets/scripts/DemoCharacter.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DemoCharacter : MonoBehaviour {
+	[SerializeField]
+	Transform actionPose;
+	
+	[SerializeField]
+	Transform idlePose;
+
+	public void SetPose(string pose) {
+		switch(pose) {
+			case "action":
+				// show the action pose
+				actionPose.gameObject.SetActive(true);
+				idlePose.gameObject.SetActive(false);
+			break;
+			default:
+				// show the normal pose
+				idlePose.gameObject.SetActive(true);
+				actionPose.gameObject.SetActive(false);
+				break;
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
-		
+		SetPose("idle");
 	}
 	
 	// Update is called once per frame
